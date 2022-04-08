a2d70bb795ea651842963be35d1d1c915196a297
elfphabet621 <elfphabet@gmail.com>
2022-04-08 11:36:22 +0700

    Revert "Reset password Email"
    This reverts commit bfa5df835d39574fee04ab0f0ef9ff9be7c397b1.

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -132,12 +132,3 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-
-#SMTP configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ginta51621@gmail.com'
-EMAIL_HOST_PASSWORD = 'zxus nzrv jsmi fiba'
-#os.environ.get('EMAIL_PASS')
